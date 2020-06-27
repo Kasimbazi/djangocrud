107ad3b756f51ac85f0e5b5ec1c8964dddcbd432
Kasimbazi <sj623041@gmail.com>
2020-06-27 08:20:01 +0300

    first commit

--- employee_register/views.py
+++ employee_register/views.py
@@ -8,10 +8,14 @@ def employee_list(request):
     context = {'employee_list':Employee.objects.all()}
     return render(request,"employee_register/employee_list.html",context)
 
-def employee_form(request):
+def employee_form(request, id=0):
     if request.method == "GET":
+        if id==0:
             form = EmployeeForm()
-            return render(request,"employee_register/employee_form.html",{'form':form})
+        else:
+                employee = Employee.objects.get(pk=id)
+                form = EmployeeForm(instance=employee)
+        return render(request,"employee_register/employee_form.html",{'form':form})
     else:
             form = EmployeeForm(request.POST)
             if form.is_valid():
